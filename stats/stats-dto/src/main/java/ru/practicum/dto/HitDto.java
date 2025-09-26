5d555a41e04cd0518b899664878ea57fc29be894
Podystal-byte <istrizancev@gmail.com>
2025-09-26 19:25:40 +0300

    Исправляю ошибки

--- stats/stats-dto/src/main/java/ru/practicum/dto/HitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/HitDto.java
@@ -1,9 +1,15 @@
 package ru.practicum.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HitDto {
     @NotBlank
     private String app;
@@ -12,5 +18,6 @@ public class HitDto {
     @NotBlank
     private String ip;
     @NotBlank
+    @JsonProperty("timestamp") // Меняем имя поля для соответствия JSON
     private String time;
 }
